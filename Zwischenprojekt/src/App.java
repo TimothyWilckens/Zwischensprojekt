8b23b6c698dcf370d925da63ad87f49fb67e6784
0w0atmeal <111504109+0w0atmeal@users.noreply.github.com>
2022-11-22 09:16:32 +0100

    hehe

--- Zwischenprojekt/src/App.java
+++ Zwischenprojekt/src/App.java
@@ -4,6 +4,8 @@ public class App {
         Mensch m1 = new Mensch("hans", "Müller", 10, false);
         Mitarbeiter ma1 = new Mitarbeiter("Max", "Mustermann", 20, false, 20000);
 
+        System.out.println("hoffentlich passiert nix");
+
         
 
     }
