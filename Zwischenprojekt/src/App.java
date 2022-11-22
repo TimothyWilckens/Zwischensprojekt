9cd3a01befa631846ebe7402e6d7a332d2e3c321
TimothyWilckens <timothy.wilckens@gymhum.de>
2022-11-22 09:31:55 +0100

    Update App.java

--- Zwischenprojekt/src/App.java
+++ Zwischenprojekt/src/App.java
@@ -1,11 +1,20 @@
-
+import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
         Mensch m1 = new Mensch("hans", "Müller", 10, false);
         Mitarbeiter ma1 = new Mitarbeiter("Max", "Mustermann", 20, false, 20000);
 
-        System.out.println("hoffentlich passiert nix");
+        ArrayList<Mensch> alleMenschen = new ArrayList<>();
+        alleMenschen.add(ma1);
 
+        Mensch[] lebendeMenschen = new Mensch[10];
+        int k = 0;
+        for(Mensch menschen : alleMenschen){
+            if(menschen.getTOT() == false){
+                lebendeMenschen[k] = menschen;
+                k++;
+            }
+        }
         
 
     }
